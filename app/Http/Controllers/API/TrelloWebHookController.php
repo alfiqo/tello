c3342169ca1839a80bafd1ee7dd0c5599d516fde
Alfiqo Jundullah <phicko93@gmail.com>
2020-10-03 13:44:59 +0700

    trello webhook

---
+++ app/Http/Controllers/API/TrelloWebHookController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\TrelloWebHook;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Http\Resources\TrelloWebHook as TrelloWebHookResource;
+
+class TrelloWebHookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return new TrelloWebHookResource(TrelloWebHook::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        TrelloWebHook::create($request->json()->all());
+
+        return response()->json([
+            'message' => 'success',
+            'status' => Response::HTTP_OK
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
