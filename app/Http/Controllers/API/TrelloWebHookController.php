a0c22a8ce5cc5cd87838f6ba5177327c0bb6fb16
Alfiqo Jundullah <phicko93@gmail.com>
2020-10-04 22:55:39 +0700

    basic action of webhook to telegram

--- app/Http/Controllers/API/TrelloWebHookController.php
+++ app/Http/Controllers/API/TrelloWebHookController.php
@@ -7,6 +7,7 @@ use App\TrelloWebHook;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use App\Http\Resources\TrelloWebHook as TrelloWebHookResource;
+use Telegram\Bot\Laravel\Facades\Telegram;
 
 class TrelloWebHookController extends Controller
 {
@@ -29,6 +30,37 @@ class TrelloWebHookController extends Controller
     public function store(Request $request)
     {
         TrelloWebHook::create($request->json()->all());
+
+        $action = $request->input('action');
+        $model = $request->input('model');
+
+        $message = '';
+        $creator = $action['memberCreator']['username'];
+        if ($action['type'] === 'updateCard') {
+            $card = $action['display']['entities']['card']['text'];
+            $listBefore = $action['display']['entities']['listBefore']['text'];
+            $listAfter = $action['display']['entities']['listAfter']['text'];
+            $message = "{$creator} move a {$card} from {$listBefore} to {$listAfter} on <a href='{$model['shortUrl']}'> {$model['name']}</a>";
+        }
+        if ($action['type'] === 'addMemberToBoard') {
+            $member = $action['display']['entities']['member']['username'];
+            $message = "{$creator} add {$member} to <a href='{$model['shortUrl']}'> {$model['name']}</a>";
+        }
+        if ($action['type'] === 'createCard') {
+            $card = $action['display']['entities']['card']['text'];
+            $list = $action['display']['entities']['list']['text'];
+            $message = "{$creator} create a card {$card} in list of {$list} on <a href='{$model['shortUrl']}'> {$model['name']}</a>";
+        }
+        if ($action['type'] === 'addMemberToCard') {
+            $card = $action['display']['entities']['card']['text'];
+            $message = "{$creator} join a {$card} on <a href='{$model['shortUrl']}'> {$model['name']}</a>";
+        }
+
+        Telegram::sendMessage([
+            'chat_id' => '-421918409',
+            'text' => $message,
+            'parse_mode' => 'HTML'
+        ]);
 
         return response()->json([
             'message' => 'success',
@@ -88,7 +120,7 @@ class TrelloWebHookController extends Controller
         $query = array(
             'key' => env('TRELLO_API_KEY'),
             'token' => env('TRELLO_TOKEN'),
-            'description'=> $request->get('name'),
+            'description' => $request->get('name'),
             'callbackURL' => $callbackUrl,
             'idModel' => $request->get('idModel')
         );
@@ -103,5 +135,4 @@ class TrelloWebHookController extends Controller
             'data' => $response
         ], Response::HTTP_OK);
     }
-
 }
